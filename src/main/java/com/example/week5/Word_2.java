9989f1db830806ba1eafd9dd23c5d67e4a235a6e
Frame <63070011@kmitl.ac.th>
2022-09-20 12:49:35 +0700

    Initial commit

---
+++ src/main/java/com/example/week5/Word_2.java
@@ -0,0 +1,13 @@
+package com.example.week5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Word_2 {
+    public ArrayList<String> badWords = new ArrayList<String>(Arrays.asList(
+            "fuck", "olo"
+    ));
+    public ArrayList<String> goodWords = new ArrayList<String>(Arrays.asList(
+            "happy", "enjoy", "like"
+    ));
+}
